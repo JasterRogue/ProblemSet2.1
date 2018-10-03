adcca94e98d750891eafe837d7a7afeda299a0cd
Ryan Madigan <t00200799@365s.ittralee.ie>
2018-10-03 16:50:49 +0100

    Start of exercise 1 in problem set 2.1

---
+++ src/BankAccount.java
@@ -0,0 +1,81 @@
+import javax.swing.*;
+
+public class BankAccount {
+
+    private String accnum;
+    private double balance;
+    private Person customer;
+
+    public void setAccnum(String accnum)
+    {
+        this.accnum = accnum;
+    }
+
+    public String getAccnum()
+    {
+        return accnum;
+    }
+
+    public void setBalance(double balance)
+    {
+        this.balance = balance;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public void setCustomer(Person customer)
+    {
+        this.customer = customer;
+    }
+
+    public Person getCustomer()
+    {
+        return customer;
+    }
+
+    public BankAccount()
+    {
+        accnum = "No Account Number";
+        balance = 0;
+        customer = new Person();
+    }
+
+    public BankAccount(String accnum, double balance, Person customer)
+    {
+        this.accnum = accnum;
+        this.balance = balance;
+        this.customer = customer;
+    }
+
+    public String toString()
+    {
+        return "\nAccount Number: " + getAccnum() +
+                "\nBalance: " + getBalance() +
+                "\nAccount Holder: " + customer.getName();
+    }
+
+    public void lodgeMoney()
+    {
+        double lodgement, bal, total;
+        bal = getBalance();
+        lodgement = Double.parseDouble(JOptionPane.showInputDialog("How much money are you entering"));
+        total = lodgement + bal;
+        setBalance(total);
+    }
+
+    public void withdrawMoney()
+    {
+        double withdraw, bal, total;
+        bal = getBalance();
+        withdraw = Double.parseDouble(JOptionPane.showInputDialog("How much money are you withdrawing"));
+        total = bal - withdraw;
+        setBalance(total);
+    }
+
+
+
+
+}
---
+++ src/BankTest.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class BankTest {
+    public static void main(String[] args)
+    {
+        BankAccount bank = new BankAccount();
+
+        JOptionPane.showMessageDialog(null,bank.toString());
+
+    }
+}
